82feb1a796669a577841662494af7c952608f43d
Tiago Carlos Batista <tiago_net_3@hotmail.com>
2025-07-28 00:34:58 -0300

    Fix bugs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import whisper
 
 model = whisper.load_model("base")
 
-result = model.transcribe(r"C:\Users\tiago\Desktop\Áudio do WhatsApp de 2025-07-25 à(s) 20.48.54_290464ce.waptt.opus",
+result = model.transcribe(r"C:\Users\tiago\Nova Era.m4a",
  language="pt", 
  task = "transcribe",
  fp16 = False,
